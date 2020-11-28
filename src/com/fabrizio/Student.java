ab107cf356379a29582df0430373f4525d8f5d80
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-28 14:13:57 -0500

    pt1

---
+++ src/com/fabrizio/Student.java
@@ -0,0 +1,20 @@
+package com.fabrizio;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+    //constructor prompts user name and year
+
+    // generate an id
+
+    // enroll in courses
+
+    // balance and tuition
+
+    // status of student
+}
